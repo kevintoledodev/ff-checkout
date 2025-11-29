98300a48afe42fa4a7ea116e9bea9e7d6a98d5ff
kevintoledodev <kevintoledo29072011@gmail.com>
2025-11-29 18:26:20 -0300

    add create-order api

--- api/create-order.js
+++ api/create-order.js
@@ -1,6 +1,7 @@
 export default async function handler(req, res) {
-  if (req.method !== "POST")
-    return res.status(405).json({ message: "Method not allowed" });
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Method not allowed" });
+  }
 
   try {
     const { name, cpf, email, amount } = req.body;
